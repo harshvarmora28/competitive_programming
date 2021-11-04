14b1b25cd14227c986daf2a8f969f4e91f7c8cc4
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-11-04 15:50:24 +0530

    question on break and loops

---
+++ test_spoj.cpp
@@ -0,0 +1,19 @@
+// Author: Harsh Varmora
+// Problem Link: https://www.spoj.com/problems/TEST/
+// Date: 4 November,2021
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    while(true){
+        int n;
+        cin>>n;
+
+        if(n == 42){
+            break;
+        }
+        cout<<n<<endl;
+    }
+}
+
