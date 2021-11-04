dcd00b59e8a7f75418688b890b2c092f2c0199ed
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2021-11-04 15:18:23 +0530

    just started with competitive programming

---
+++ flow006.cpp
@@ -0,0 +1,27 @@
+// Author: Harsh Varmora
+// Problem Link: https://www.codechef.com/problems/FLOW006
+// Date: 4 November,2021
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        int digit_sum = 0;
+
+        while(n > 0){
+            int last_digit = n % 10;
+            digit_sum = digit_sum + last_digit;
+            n = n / 10;
+        }
+        cout << digit_sum << endl;
+    }
+
+    return 0;
+}
+
