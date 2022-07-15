6ba54c25c295ca944d671eb765e7694bbeb21b84
Harsh Varmora <harsh.vce20@sot.pdpu.ac.in>
2022-07-15 18:21:06 +0530

    updated cpp template

--- Template/cpp_template.cpp
+++ Template/cpp_template.cpp
@@ -34,19 +34,11 @@ typedef vector<pl>		vpl;
 typedef vector<vi>		vvi;
 typedef vector<vl>		vvl;
 mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
-int rng(int lim) {
-	uniform_int_distribution<int> uid(0,lim-1);
-	return uid(rang);
-}
 int mpow(int base, int exp); 
 void ipgraph(int n, int m);
 void dfs(int u, int par);
-
 const int mod = 1'000'000'007;
 const int N = 3e5, M = N;
-
-vi g[N];
-int a[N];
 
 void solve() {
     
@@ -55,40 +47,10 @@ void solve() {
 int main() {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     srand(chrono::high_resolution_clock::now().time_since_epoch().count());
-
     int t = 1;
     cin >> t;
     while(t--) {
       solve();
     }
-
     return 0;
-}
-
-int mpow(int base, int exp) {
-  base %= mod;
-  int result = 1;
-  while (exp > 0) {
-    if (exp & 1) result = ((ll)result * base) % mod;
-    base = ((ll)base * base) % mod;
-    exp >>= 1;
-  }
-  return result;
-}
-
-void ipgraph(int n, int m){
-	int i, u, v;
-	while(m--){
-		cin>>u>>v;
-    u--, v--;
-		g[u].pb(v);
-		g[v].pb(u);
-	}
-}
-
-void dfs(int u, int par){
-	for(int v:g[u]){
-		if (v == par) continue;
-		dfs(v, u);
-	}
 }
